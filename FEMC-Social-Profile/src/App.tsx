3a5930bdfaa60b4b76fa020b6ba9914848fdab3f
Anton Garay <antonvince.garay@gmail.com>
2024-07-25 17:24:25 +1200

    Added button hover effect

--- FEMC-Social-Profile/src/App.tsx
+++ FEMC-Social-Profile/src/App.tsx
@@ -1,16 +1,15 @@
 import { Button } from "./components/Button"
 
 function App() {
-
   return (
     <>
       <div className='bg-[#141414] w-dvw h-dvh flex justify-center items-center'>
         <div className="bg-[#1F1F1F] w-[384px] h-[615px] p-[40px] rounded-lg flex font-[inter] flex-col items-center">
           <img src="avatar-jessica.jpeg" className="rounded-full w-[90px] h-[90px]"></img>
           <p className="text-white text-[24px] font-medium mt-[20px]">Jessica Randal</p>
-          <p className="text-[hsl(75,94%,57%)] font-medium text-[14px]">London, United Kingdom</p>
-          <p className="text-white text-[14px]">"Front-end developer and avid reader"</p>
-          <div className="flex flex-col gap-4">
+          <p className="text-[hsl(75,94%,57%)] font-medium text-[14px] mt-[5px]">London, United Kingdom</p>
+          <p className="text-white text-[14px] mt-[23px]">"Front-end developer and avid reader"</p>
+          <div className="flex flex-col gap-4 mt-[23px]">
             <Button text="Github"></Button>
             <Button text="Frontend Mentor"></Button>
             <Button text="LinkedIn"></Button>
--- FEMC-Social-Profile/src/components/Button.tsx
+++ FEMC-Social-Profile/src/components/Button.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
-
 export const Button = ({text}: {text: string}) => {
   return (
-    <button className='w-[305px] h-[44px] bg-[#333333] rounded-lg text-white'>{text}</button>
+    <button className='font-medium  w-[305px] h-[44px] bg-[#333333] rounded-lg text-white hover:bg-[hsl(75,94%,57%)]  transition-colors duration-300 hover:text-black'>{text}</button>
   )
 }
